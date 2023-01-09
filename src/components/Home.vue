b7d9e6b9d87240afb37b2e6283ecfaf8594a0692
eBaBy-C <udbrkve@gmail.com>
2023-01-09 13:23:57 +0800

    引入router

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,14 @@
 <template>
-  <h1>home</h1>
+  <div>
+    <h1>im home </h1>
+    <p>最近在看的电影叫做"一一"</p>
+  </div>  
 </template>
+
+<style scoped>
+  div {
+    width: 100%;
+    height: 100%;
+    border: 1px solid;
+  }  
+</style>
---
+++ src/router.js
@@ -0,0 +1,10 @@
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+
+Vue.use(VueRouter)
+const routers = [
+  {
+    path: '/',
+    component
+  }
+]
